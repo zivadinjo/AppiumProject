ca78ddb5fafccb9f2e070c382b25f186976f145b
zivadinjo <zivadinjo1985@gmail.com>
2022-09-18 11:35:55 -0500

    SauceLab Test

--- src/test/java/com/cydeo/test/SwagLabTest.java
+++ src/test/java/com/cydeo/test/SwagLabTest.java
@@ -1,9 +1,14 @@
 package com.cydeo.test;
 
+import com.cydeo.utils.ConfigurationReader;
 import com.cydeo.utils.Driver;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SwagLabTest {
 
@@ -16,6 +21,22 @@ public class SwagLabTest {
         System.out.println("driver.getDeviceTime() = " + driver.getDeviceTime());
 
         Thread.sleep(3000);
+
+        WebElement username = driver.findElementByAccessibilityId("test-Username");
+        username.sendKeys(ConfigurationReader.getProperty("swagUser"));
+        WebElement password = driver.findElementByAccessibilityId("test-Password");
+        password.sendKeys(ConfigurationReader.getProperty("swagPass"));
+        WebElement login = driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]/android.widget.TextView");
+        login.click();
+
+        Thread.sleep(2000);
+
+        MobileElement products = driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-Cart drop zone\"]/android.view.ViewGroup"));
+
+        Assertions.assertTrue(products.isDisplayed());
+
+
+
 
         Driver.closeDriver();
 
